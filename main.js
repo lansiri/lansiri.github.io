36714a73ce619c1cb9a10c51c146705e11d65b69
lansiri <riki.lansilahti@gmail.com>
2024-10-09 09:23:15 +0300

    changes

---
+++ main.js
@@ -0,0 +1,5 @@
+// Hamburger menu functionality
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('open');
+});
